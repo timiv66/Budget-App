beaecd17cf9cbef0c978300f21ff217e6534ee7c
Timi Verissimo <timiv9@gmail.com>
2024-01-09 17:33:02 -0500

    monthly income section

--- src/budget/BudgetFX.java
+++ src/budget/BudgetFX.java
@@ -1,20 +1,28 @@
 package budget;
 
+import java.util.function.UnaryOperator;
+
 import javafx.application.Application;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
-import javafx.scene.Group;
+import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.control.TextFormatter;
 import javafx.scene.effect.DropShadow;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.Pane;
+import javafx.scene.layout.VBox;
+import javafx.scene.paint.Color;
 import javafx.scene.text.Font;
 import javafx.scene.text.Text;
 import javafx.scene.text.TextAlignment;
 import javafx.stage.Stage;
+import javafx.util.converter.IntegerStringConverter;
 
 public class BudgetFX extends Application{
 	
@@ -24,6 +32,8 @@ public class BudgetFX extends Application{
 	private double savings;
 	
 	DropShadow shadow = new DropShadow();
+	Font openFont = new Font("Impact",22);
+	Font txtFont = new Font("Times New Roman",12);
 	
 	public static void main(String[] args) {
 		launch();
@@ -35,15 +45,15 @@ public class BudgetFX extends Application{
 		//welcome screen
 		Pane p1 = new Pane();
 		Scene t = new Scene(p1,500,400);
-		
+		t.setRoot(openScreen(t));
 		mainStage.setTitle("Budget App");
-		
 		Image icon = new Image("https://cdn-icons-png.flaticon.com/512/781/781831.png");
 		mainStage.getIcons().add(icon);
-		
-		Font openFont = new Font("Impact",22);
-		Font txtFont = new Font("Times New Roman",12);
-		
+		mainStage.setScene(t);
+		mainStage.show();	
+	}
+	
+	public Pane openScreen(Scene t) {
 		Text welcomeTxt = new Text("Welcome to 50/30/20 Budget Calculator");
 		welcomeTxt.setTextAlignment(TextAlignment.CENTER);
 		welcomeTxt.setX(65);
@@ -60,43 +70,143 @@ public class BudgetFX extends Application{
 		openImg.setX(95);
 		openImg.setY(150);
 		
-		Button nxtBtn = new Button("Next");
-		nxtBtn.setTranslateX(220);
-		nxtBtn.setTranslateY(340);
+		Button nxtBtn1 = new Button("Next");
 		
-		nxtBtn.setOnAction(new EventHandler <ActionEvent>() {
+		nxtBtn1.setOnAction(new EventHandler <ActionEvent>() {
 			@Override
 			public void handle(ActionEvent arg0) {
 				t.setRoot(income(t));
 			}
 		});
-		
-		nxtBtn.addEventHandler(MouseEvent.MOUSE_ENTERED, new EventHandler<MouseEvent>() {
+		nxtBtn1.addEventHandler(MouseEvent.MOUSE_ENTERED, new EventHandler<MouseEvent>() {
 	          @Override
 	          public void handle(MouseEvent e) {
-	            nxtBtn.setEffect(shadow);
+	            nxtBtn1.setEffect(shadow);
 	          }
 	        });
-
-	    nxtBtn.addEventHandler(MouseEvent.MOUSE_EXITED,new EventHandler<MouseEvent>() {
+	    nxtBtn1.addEventHandler(MouseEvent.MOUSE_EXITED,new EventHandler<MouseEvent>() {
 	          @Override
 	          public void handle(MouseEvent e) {
-	            nxtBtn.setEffect(null);
+	            nxtBtn1.setEffect(null);
 	          }
 	        });
-
-		p1.getChildren().addAll(welcomeTxt,adviceTxt,openImg,nxtBtn);
-		
-		
-		mainStage.setScene(t);
-		mainStage.show();
+	    
+	    VBox vb1 = new VBox(20);
+	    vb1.setAlignment(Pos.CENTER);
+	    vb1.getChildren().addAll(welcomeTxt,adviceTxt,openImg,nxtBtn1);
+	    return vb1;
 	}
 	
 	public Pane income(Scene t) {
+		//only numbers accepted in txt fields
+		UnaryOperator<TextFormatter.Change> filter = change -> {
+	        String text = change.getText();
+	        // Check if the new text is a number or empty
+	        if (text.matches("[0-9]*") || text.isEmpty()) {
+	            return change;
+	        }
+	        // If not a number or empty, reject the change
+	        return null;
+	    };
+	    TextFormatter<Integer> textFormatter1 = new TextFormatter<>(new IntegerStringConverter(), null, filter);
 		
+		t.getWindow().setHeight(145);
+		t.getWindow().setWidth(360);
 		
+		Text instructTxt = new Text("Please input your monthly income after taxes");
+		instructTxt.setFont(txtFont);
+		instructTxt.setX(5);
+		instructTxt.setY(15);
+		
+		Label monthlyIncLbl = new Label("Monthly Income:");
+		monthlyIncLbl.setTranslateX(3);
+		monthlyIncLbl.setTranslateY(20);
+		monthlyIncLbl.setFont(openFont);
+		
+		Label moneyLbl = new Label("$");
+		moneyLbl.setTranslateX(161);
+		moneyLbl.setTranslateY(21);
+		moneyLbl.setFont(openFont);
+		
+		TextField incTxtF = new TextField();
+		incTxtF.setTranslateX(175);
+		incTxtF.setTranslateY(24);
+		incTxtF.setTextFormatter(textFormatter1);
+		
+		Text errorMsg = new Text("");
+		errorMsg.setFill(Color.RED);
+		errorMsg.setFont(txtFont);
+		errorMsg.setX(5);
+		errorMsg.setY(60);
+		errorMsg.setVisible(false);
+		
+        Button nxtBtn2 = new Button("Next");
+        nxtBtn2.setTranslateX(300);
+        nxtBtn2.setTranslateY(78);
+        
+		//Next button actions
+		nxtBtn2.setOnAction(new EventHandler <ActionEvent>() {
+			@Override
+			public void handle(ActionEvent arg0) {
+			  if (incTxtF.getText().isBlank()) {
+				errorMsg.setText("Please enter a valid number");
+			  	errorMsg.setVisible(true);
+			  }
+			  else{
+				monthlyIncome = Double.parseDouble(incTxtF.getText());
+				t.setRoot(needs(t));
+			  }
+			}
+		});
+		nxtBtn2.addEventHandler(MouseEvent.MOUSE_ENTERED, new EventHandler<MouseEvent>() {
+	          @Override
+	          public void handle(MouseEvent e) {
+	            nxtBtn2.setEffect(shadow);
+	          }
+	        });
+	    nxtBtn2.addEventHandler(MouseEvent.MOUSE_EXITED,new EventHandler<MouseEvent>() {
+	          @Override
+	          public void handle(MouseEvent e) {
+	            nxtBtn2.setEffect(null);
+	          }
+	        });
+	    
+	    Button resetBtn = new Button("Reset");
+	    resetBtn.setTranslateX(2);
+	    resetBtn.setTranslateY(78);
+	    
+	    resetBtn.setOnAction(new EventHandler <ActionEvent>() {
+			@Override
+			public void handle(ActionEvent arg0) {
+			  monthlyIncome = 0;
+			  t.setRoot(openScreen(t));
+			  t.getWindow().setWidth(500);
+			  t.getWindow().setHeight(400);
+			}
+		});
+	    
+	    resetBtn.addEventHandler(MouseEvent.MOUSE_ENTERED, new EventHandler<MouseEvent>() {
+	          @Override
+	          public void handle(MouseEvent e) {
+	           resetBtn.setEffect(shadow);
+	          }
+	        });
+	    resetBtn.addEventHandler(MouseEvent.MOUSE_EXITED,new EventHandler<MouseEvent>() {
+	          @Override
+	          public void handle(MouseEvent e) {
+	            resetBtn.setEffect(null);
+	          }
+	        });
+	    
 		Pane incomePane = new Pane();
+		incomePane.getChildren().addAll(monthlyIncLbl,instructTxt,moneyLbl,incTxtF,errorMsg,nxtBtn2,resetBtn);
 		return incomePane;
+	}
+	
+	public Pane needs(Scene t) {
+		
+		Pane needsPane = new Pane();
+		return needsPane;
 	}
 
 }
